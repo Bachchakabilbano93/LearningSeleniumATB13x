931a39e6f955fdca5b3879ced3ead3deef984b23
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-09-10 05:30:39 +0530

    8th September 2025 Class How to Handle Input box, checkbox, radio box and modal

--- src/test/java/com/abir/ex06_Input_Select_Alerts_Radio_Checkbox/TestSelenium26_Input_Checkbox_Radio.java
+++ src/test/java/com/abir/ex06_Input_Select_Alerts_Radio_Checkbox/TestSelenium26_Input_Checkbox_Radio.java
@@ -1,6 +1,8 @@
 package com.abir.ex06_Input_Select_Alerts_Radio_Checkbox;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestSelenium26_Input_Checkbox_Radio {
@@ -8,6 +10,27 @@ public class TestSelenium26_Input_Checkbox_Radio {
 
         WebDriver driver = new ChromeDriver();
         driver.get("https://awesomeqa.com/practice.html");
+
+        // Input
+        WebElement firstname = driver.findElement(By.name("firstname"));
+        WebElement firstname1 = driver.findElement(By.xpath("//input[@name='firstname']"));
+        firstname.sendKeys("the testing academy");
+
+        // Radio Button
+        WebElement radiobox = driver.findElement(By.id("exp-1"));
+        radiobox.click();
+
+        // checkbox
+        WebElement checkbox = driver.findElement(By.id("profession-0"));
+        checkbox.click();
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        // CHeck Box
+        driver.quit();
 
     }
 }
