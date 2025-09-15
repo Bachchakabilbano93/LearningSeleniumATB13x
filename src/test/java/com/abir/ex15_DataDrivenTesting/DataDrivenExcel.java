a7a4259ca316dabf33ac20bcef91f2dede1be629
Bachchakabilbano93 <abirbhattacharya7@gmail.com>
2025-09-15 16:41:53 +0530

    12th September 2025 Class Data Driven Testing Recording

---
+++ src/test/java/com/abir/ex15_DataDrivenTesting/DataDrivenExcel.java
@@ -0,0 +1,17 @@
+package com.abir.ex15_DataDrivenTesting;
+
+import org.testng.annotations.Test;
+
+public class DataDrivenExcel {
+
+    @Test(dataProvider = "getData",dataProviderClass = UtilExcel.class)
+    public void test_VWOLogin(String email, String password){
+        System.out.println("Email - "+ email);
+        System.out.println("Password - "+ password);
+
+        // If email and pass -> correct ->
+
+        // Else
+
+    }
+}
